8f33c8869e10dda0fc91555e866d58e8b45391b5
Subhan <muhammedsubhan692@gmail.com>
2023-07-09 16:44:42 +0500

    trending-products

--- src/Components/TrendingProductsCard/TrendingProductsCard.jsx
+++ src/Components/TrendingProductsCard/TrendingProductsCard.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useProductById } from "../../Pages/ProductsContext/ProductsContext";
+import { useNavigate } from "react-router-dom";
 
 const TrendingProductsCard = ({ data }) => {
+  const { setProductById } = useProductById();
+
+  const navigate = useNavigate();
+
+  const handleTrendingDetails = () => {
+    setProductById(data);
+    navigate("/product-details");
+  };
+
   return (
     <>
-      <div className="flex flex-col    px-4 py-2 rounded-xl  bg-black/[.06] cursor-pointer gap-3">
+      <div
+        onClick={handleTrendingDetails}
+        className="flex flex-col    px-4 py-2 rounded-xl  bg-black/[.06] cursor-pointer gap-3"
+      >
         <div className="flex flex-col justify-between items-center gap-3 xs:flex-wrap xs:justify-center sm:flex-nowrap sm:justify-between">
           <div className="flex justify-between px-2 w-72">
             <h2 className="text-xl xs:text-base sm:text-xl font-bold">
